5dee0dac3ec83dc4b94c3f9a1d606ab186a9001b
ismail <ismail.bouasria@laplateforme.io>
2025-10-17 09:14:37 +0200

    feature: add display photo list

--- src/app/service/photo-service.ts
+++ src/app/service/photo-service.ts
@@ -3,16 +3,31 @@ import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera
 import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Preferences } from '@capacitor/preferences';
 
+
+// Interface UserPhoto
+export interface UserPhoto {
+  filepath: string;
+  webviewPath?: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class PhotoService {
+
+  public photos: UserPhoto[] =[];
   public async addNewToGallery() {
   // Take a photo
   const capturedPhoto = await Camera.getPhoto({
     resultType: CameraResultType.Uri,
     source: CameraSource.Camera,
     quality: 100
+
+  });
+
+   this.photos.unshift({
+    filepath: "soon...",
+    webviewPath: capturedPhoto.webPath!
   });
 }
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonFabButton, IonFab, IonIcon } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonFabButton, IonFab, IonIcon, IonGrid, IonRow, IonCol, IonImg } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
  import { PhotoService } from '../service/photo-service';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonIcon, IonFab, IonFabButton, IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonImg, IonCol, IonRow, IonGrid, IonIcon, IonFab, IonFabButton, IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
 })
 export class Tab1Page {
 
