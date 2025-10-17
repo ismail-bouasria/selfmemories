92bb0c4cf803e6c10df0e9a8edd311fd05911381
ismail <ismail.bouasria@laplateforme.io>
2025-10-17 09:40:43 +0200

    feature: display and fix loaded photo

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,19 +1,46 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonFabButton, IonFab, IonIcon, IonGrid, IonRow, IonCol, IonImg } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonFabButton,
+  IonFab,
+  IonIcon,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonImg,
+} from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
- import { PhotoService } from '../service/photo-service';
+import { PhotoService } from '../service/photo-service';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonImg, IonCol, IonRow, IonGrid, IonIcon, IonFab, IonFabButton, IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [
+    IonImg,
+    IonCol,
+    IonRow,
+    IonGrid,
+    IonIcon,
+    IonFab,
+    IonFabButton,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    ExploreContainerComponent,
+  ],
 })
 export class Tab1Page {
+  constructor(public photoService: PhotoService) {}
 
+  async ngOnInit() {
+    await this.photoService.loadSaved();
+  }
 
-constructor(public photoService: PhotoService) { }
-
-addPhotoToGallery() {
-  this.photoService.addNewToGallery();
-}
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
 }
